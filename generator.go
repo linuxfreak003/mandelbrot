58182d7eaa1f07c2738475ebda6ee5a390b26baf
Jared Chapman <jared.chapman@tcnbroadcasting.com>
2020-08-12 14:17:17 -0600

    implement colorization as a function

--- generator.go
+++ generator.go
@@ -7,6 +7,8 @@ import (
 	"io"
 )
 
+type ColorFunc func(int) color.RGBA
+
 type Generator struct {
 	Width      int
 	Height     int
@@ -17,6 +19,22 @@ type Generator struct {
 	Image      *image.RGBA
 	ColorStart color.RGBA
 	ColorEnd   color.RGBA
+	Colorize   ColorFunc
+}
+
+func DefaultColorize(iter int) color.RGBA {
+	if iter == -1 {
+		return color.RGBA{0, 0, 0, 0xff}
+	}
+
+	return color.RGBA{
+		uint8(iter % 255),
+		uint8(iter % 255),
+		uint8(iter % 255),
+		0xff,
+	}
+	// percentage := float64(iter) / float64(g.Limit)
+	// return Gradient(g.ColorStart, g.ColorEnd, percentage)
 }
 
 func NewGenerator(width, height int, x, y, zoom float64) *Generator {
@@ -30,15 +48,17 @@ func NewGenerator(width, height int, x, y, zoom float64) *Generator {
 		Image:      image.NewRGBA(image.Rect(0, 0, width, height)),
 		ColorStart: color.RGBA{0, 0, 0, 0xff},
 		ColorEnd:   color.RGBA{255, 255, 255, 0xff},
+		Colorize:   DefaultColorize,
 	}
 }
 
-func (g *Generator) SetWidth(width int)   { g.Width = width }
-func (g *Generator) SetHeight(height int) { g.Height = height }
-func (g *Generator) SetX(x float64)       { g.X = x }
-func (g *Generator) SetY(y float64)       { g.Y = y }
-func (g *Generator) SetZoom(zoom float64) { g.Zoom = zoom }
-func (g *Generator) SetLimit(limit int)   { g.Limit = limit }
+func (g *Generator) SetWidth(width int)       { g.Width = width }
+func (g *Generator) SetHeight(height int)     { g.Height = height }
+func (g *Generator) SetX(x float64)           { g.X = x }
+func (g *Generator) SetY(y float64)           { g.Y = y }
+func (g *Generator) SetZoom(zoom float64)     { g.Zoom = zoom }
+func (g *Generator) SetLimit(limit int)       { g.Limit = limit }
+func (g *Generator) SetColorize(cf ColorFunc) { g.Colorize = cf }
 
 func (g *Generator) Generate() error {
 	wInc := 4.0 / float64(g.Width)
@@ -56,11 +76,7 @@ func (g *Generator) Generate() error {
 
 func (g *Generator) GetColor(x, y float64) color.RGBA {
 	iter := CalcPoint(x, y, g.Limit)
-	if iter == -1 {
-		return g.ColorStart
-	}
-	percentage := float64(iter) / float64(g.Limit)
-	return Gradient(g.ColorStart, g.ColorEnd, percentage)
+	return g.Colorize(iter)
 }
 
 func (g *Generator) Write(w io.Writer) error {
